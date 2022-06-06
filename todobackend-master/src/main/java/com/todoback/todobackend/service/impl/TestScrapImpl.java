67b8107a360dee3594ec0c6fc05c70efa3af50d8
TurtleBeatboxer <mwma9999@gmail.com>
2022-06-07 00:05:44 +0200

    dupa

--- todobackend-master/src/main/java/com/todoback/todobackend/service/impl/TestScrapImpl.java
+++ todobackend-master/src/main/java/com/todoback/todobackend/service/impl/TestScrapImpl.java
@@ -21,13 +21,13 @@ public class TestScrapImpl implements TestScrap {
     private boolean cookiesDone;
 
     public TestScrapImpl(ChromeDriver driver) {
-<<<<<<< HEAD
-        this.driver = driver; }
+        this.driver = driver;
+    }
 
     public void scrapData() {
-            driver.navigate().to("https://mobalytics.gg/");
-            WebElement bodyCookies = driver.findElement(By.tagName("body"));
-            System.out.println(driver.getCurrentUrl());
+        driver.navigate().to("https://mobalytics.gg/");
+        WebElement bodyCookies = driver.findElement(By.tagName("body"));
+        System.out.println(driver.getCurrentUrl());
       /*      WebElement acceptButton = bodyCookies.findElement(By.xpath("//button[@aria-label='Accept all']"));
             acceptButton.click();
             System.out.println(driver.getCurrentUrl());
@@ -37,141 +37,141 @@ public class TestScrapImpl implements TestScrap {
             List<WebElement> thumbnailLink = bodyGames.findElements(By.xpath("//img[@class='style-scope yt-img-shadow']"));
             printWebElements(gameTitle);
             printWebElements(viewerCount);*/
-        }
-        public void printWebElements(List<WebElement> webElementList) {
-            List<String> stringToOutput = new ArrayList<>();
-            for (int i = 0; i < webElementList.size(); i++) {
-                if (!webElementList.get(i).getText().isEmpty()) {
-                    stringToOutput.add(webElementList.get(i).getText());
-=======
-        this.driver = driver;
-        this.cookiesDone = false;
     }
 
-    public List<RecentActivity> scrapData(WebDriverWait wait, WebElement body) {
-        List<RecentActivity> RecentActivity = getRecentActivity(wait, body);
-        printActivity(RecentActivity);
-        return RecentActivity;
-    }
+    public void printWebElements(List<WebElement> webElementList) {
+        List<String> stringToOutput = new ArrayList<>();
+        for (int i = 0; i < webElementList.size(); i++) {
+            if (!webElementList.get(i).getText().isEmpty()) {
+                stringToOutput.add(webElementList.get(i).getText());
+                this.driver = driver;
+                this.cookiesDone = false;
+            }
+
+            public List<RecentActivity> scrapData (WebDriverWait wait, WebElement body){
+                List<RecentActivity> RecentActivity = getRecentActivity(wait, body);
+                printActivity(RecentActivity);
+                return RecentActivity;
+            }
 
 
+            public WinRateDTO scrapWinRate (WebDriverWait wait, WebElement body){
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[3]/div[1]/span[4]/span")));
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[3]/div[1]/span[4]/span")));
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div/div[2]/div[1]/span[4]/span")));
+                WebElement rankedSolo = body.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[3]/div[1]/span[4]/span"));
+                WebElement rankedFlex = body.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[3]/div[1]/span[4]/span"));
+                WebElement normalDraft = body.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div/div[2]/div[1]/span[4]/span"));
+                WinRateDTO data = new WinRateDTO();
+                data.setRankedSolo(rankedSolo.getText());
+                data.setRankedFlex(rankedFlex.getText());
+                data.setNormalDraft(normalDraft.getText());
+                return data;
 
-    public WinRateDTO scrapWinRate(WebDriverWait wait, WebElement body){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[3]/div[1]/span[4]/span")));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[3]/div[1]/span[4]/span")));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div/div[2]/div[1]/span[4]/span")));
-        WebElement rankedSolo = body.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[3]/div[1]/span[4]/span"));
-        WebElement rankedFlex = body.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[3]/div[1]/span[4]/span"));
-        WebElement normalDraft = body.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div/div[2]/div[1]/span[4]/span"));
-        WinRateDTO data = new WinRateDTO();
-        data.setRankedSolo(rankedSolo.getText());
-        data.setRankedFlex(rankedFlex.getText());
-        data.setNormalDraft(normalDraft.getText());
-        return data;
-
-    }
+            }
 
 
-    public List<RecentActivity> getRecentActivity(WebDriverWait wait, WebElement body){
-        List<RecentActivity> ActivityList = new ArrayList<>();
-        body.sendKeys(Keys.PAGE_DOWN);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
-                "/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[5]/div[1]/div[1]/div/*[name()='svg']")));
-        WebElement table = this.driver.findElement(By.xpath(
-                "/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[5]/div[1]/div[1]/div/*[name()='svg']/*[name()='g'][2]"));
-        List<WebElement> columns = table.findElements(By.xpath("./*"));
+            public List<RecentActivity> getRecentActivity (WebDriverWait wait, WebElement body){
+                List<RecentActivity> ActivityList = new ArrayList<>();
+                body.sendKeys(Keys.PAGE_DOWN);
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+                        "/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[5]/div[1]/div[1]/div/*[name()='svg']")));
+                WebElement table = this.driver.findElement(By.xpath(
+                        "/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[5]/div[1]/div[1]/div/*[name()='svg']/*[name()='g'][2]"));
+                List<WebElement> columns = table.findElements(By.xpath("./*"));
 
-        for (int i = 1; i <= columns.size(); i++) {
-            String path = String.format(
-                    "/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[5]/div[1]/div[1]/div/*[name()='svg']/*[name()='g'][2]/*[name()='g'][%d]",
-                    i);
-            WebElement row = this.driver.findElement(By.xpath(path));
-            List<WebElement> rowNumber = row.findElements(By.xpath("./*"));
-            for (int j = 1; j <= rowNumber.size(); j++) {
+                for (int i = 1; i <= columns.size(); i++) {
+                    String path = String.format(
+                            "/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[5]/div[1]/div[1]/div/*[name()='svg']/*[name()='g'][2]/*[name()='g'][%d]",
+                            i);
+                    WebElement row = this.driver.findElement(By.xpath(path));
+                    List<WebElement> rowNumber = row.findElements(By.xpath("./*"));
+                    for (int j = 1; j <= rowNumber.size(); j++) {
 
-                String string = String.format(
-                        "/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[5]/div[1]/div[1]/div/*[name()='svg']/*[name()='g'][2]/*[name()='g'][%d]/*[name()='rect'][%2d]",
-                        i, j);
-                this.driver.findElement(By.xpath(string)).click();
-                String attribute = this.driver.findElement(By.xpath(string)).getAttribute("aria-describedby");
-                if (attribute != null) {
-                    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(attribute)));
-                    WebElement e = body.findElement(By.id(attribute));
-                    List<WebElement> el = e.findElements(By.xpath("./*"));
-                    RecentActivity rect = new RecentActivity();
-                    String d = el.get(0).getText();
-                    String data[] = d.split("\\r?\\n");
-                    rect.setDate(data[1]);
-                    rect.setHoursPlayed(data[3]);
-                    rect.setGamesRatio(data[5]);
-                    rect.setWinRatioByString(data[7]);
-                    ActivityList.add(rect);
+                        String string = String.format(
+                                "/html/body/div[1]/div[1]/div[1]/div[4]/div/div/main/div[1]/div[5]/div[1]/div[1]/div/*[name()='svg']/*[name()='g'][2]/*[name()='g'][%d]/*[name()='rect'][%2d]",
+                                i, j);
+                        this.driver.findElement(By.xpath(string)).click();
+                        String attribute = this.driver.findElement(By.xpath(string)).getAttribute("aria-describedby");
+                        if (attribute != null) {
+                            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(attribute)));
+                            WebElement e = body.findElement(By.id(attribute));
+                            List<WebElement> el = e.findElements(By.xpath("./*"));
+                            RecentActivity rect = new RecentActivity();
+                            String d = el.get(0).getText();
+                            String data[] = d.split("\\r?\\n");
+                            rect.setDate(data[1]);
+                            rect.setHoursPlayed(data[3]);
+                            rect.setGamesRatio(data[5]);
+                            rect.setWinRatioByString(data[7]);
+                            ActivityList.add(rect);
 
->>>>>>> a6d617690b41853ccaa261f49b1da51764ce90d0
+                        }
+
+                    }
                 }
+                return ActivityList;
+            }
+
+            public void printActivity (List < RecentActivity > rect) {
+                List<String> stringToOutput = new ArrayList<>();
+                for (int i = 0; i < rect.size(); i++) {
+
+                    stringToOutput.add(rect.get(i).getDate());
+                    stringToOutput.add(rect.get(i).getGamesRatio());
+                    stringToOutput.add(rect.get(i).getHoursPlayed());
+                    stringToOutput.add(rect.get(i).getWinRatio());
+
+                }
+                for (int i = 0; i < stringToOutput.size(); i++) {
+                    System.out.println(stringToOutput.get(i));
+                }
+            }
+
+            public void acceptCookies (WebDriverWait wait, WebElement body){
+
+                wait.until(ExpectedConditions.elementToBeClickable(
+                        By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/header/div[1]/div/div/div[2]/div/button")));
+                WebElement refreshButton = body.findElement(
+                        By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/header/div[1]/div/div/div[2]/div/button"));
+                refreshButton.click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Consent']")));
+                WebElement cookieButton = body.findElement(By.xpath("//button[@aria-label='Consent']"));
+                cookieButton.click();
+
+
+                this.cookiesDone = true;
+
+
+            }
+
+            public WebDriverWait setWait () {
+                WebDriverWait wait = new WebDriverWait(driver, 10);
+                return wait;
+            }
+
+            public void getMobalytics (String username, String lolServer){
+                String path = String.format("https://app.mobalytics.gg/lol/profile/%s/%s/overview", lolServer, username);
+                System.out.println(path);
+                this.driver.manage().window().maximize();
+                this.driver.navigate().to(path);
+
+            }
+
+            public LOLUserDATA getLOLUserDATA (String lolServer, String lolUsername){
+
+                getMobalytics(lolUsername, lolServer);
+                WebDriverWait wait = setWait();
+                WebElement body = driver.findElement(By.tagName("body"));
+                if (this.cookiesDone == false) {
+                    acceptCookies(wait, body);
+                }
+                LOLUserDATA data = new LOLUserDATA();
+                data.setActivityList(scrapData(wait, body));
+                data.setUserWinrate(scrapWinRate(wait, body));
+                return data;
 
             }
         }
-        return ActivityList;
-    }
-
-    public void printActivity(List<RecentActivity> rect) {
-        List<String> stringToOutput = new ArrayList<>();
-            for (int i = 0; i < rect.size(); i++) {
-
-                stringToOutput.add(rect.get(i).getDate());
-                stringToOutput.add(rect.get(i).getGamesRatio());
-            stringToOutput.add(rect.get(i).getHoursPlayed());
-            stringToOutput.add(rect.get(i).getWinRatio());
-
-        }
-        for (int i = 0; i < stringToOutput.size(); i++) {
-            System.out.println(stringToOutput.get(i));
-        }
-    }
-
-    public void acceptCookies(WebDriverWait wait, WebElement body){
-
-        wait.until(ExpectedConditions.elementToBeClickable(
-                By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/header/div[1]/div/div/div[2]/div/button")));
-        WebElement refreshButton = body.findElement(
-                By.xpath("/html/body/div[1]/div[1]/div[1]/div[4]/div/div/header/div[1]/div/div/div[2]/div/button"));
-        refreshButton.click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Consent']")));
-        WebElement cookieButton = body.findElement(By.xpath("//button[@aria-label='Consent']"));
-        cookieButton.click();
-
-
-    this.cookiesDone = true;
-
-
-    }
-
-    public WebDriverWait setWait(){
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        return wait;
-    }
-
-    public void getMobalytics(String username, String lolServer){
-        String path = String.format("https://app.mobalytics.gg/lol/profile/%s/%s/overview", lolServer, username);
-        System.out.println(path);
-        this.driver.manage().window().maximize();
-        this.driver.navigate().to(path);
-
-    }
-
-    public LOLUserDATA getLOLUserDATA(String lolServer, String lolUsername){
-
-        getMobalytics(lolUsername, lolServer);
-        WebDriverWait wait = setWait();
-        WebElement body = driver.findElement(By.tagName("body"));
-        if(this.cookiesDone == false){
-            acceptCookies(wait, body);
-        }
-        LOLUserDATA data = new LOLUserDATA();
-        data.setActivityList(scrapData(wait, body));
-        data.setUserWinrate(scrapWinRate(wait, body));
-        return data;
-
     }
 }
